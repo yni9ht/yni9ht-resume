bceb14fdd1586263754a066b4eb563b5c020d01c
yni9ht <ysz657389517@gmail.com>
2023-04-13 12:03:33 +0800

    add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,16 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   vite: {
     ssr: {
-      external: ['svgo'],
-    },
+      external: ['svgo']
+    }
   },
+  output: "server",
+  adapter: vercel()
 });
